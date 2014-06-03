6089bba7c886b9d5df5e9d02141a436c93501261
Serge <xeliffer@gmail.com>
2014-06-03 16:16:53 +0400

    Create gcd.h

---
+++ gcd.h
@@ -0,0 +1,12 @@
+// gcd.h
+//Extended euclidean algorithm:
+//GCD - Greatest Common Divisor
+//ex - extended
+int pgcd (int a, int b, int & x, int & y) ;
+//Euclidean algorithm:
+//GCD - Greatest Common Divisor
+int gcd (int a, int b) ;
+//Lowest common denominator
+int lcd (int a, int b) ;
+//binary pow
+int binpow (int a, int n);
